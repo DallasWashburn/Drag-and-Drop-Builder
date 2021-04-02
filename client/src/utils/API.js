59f756b0ecdf0f65a81c65ba84d32940e42b18ea
Dallas Washburn <dallas@cybermark.com>
2021-04-02 13:56:18 -0700

    deploment changes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,19 +5,19 @@ export default {
 
   // Gets all users
   getUsers: async function () {
-    let response = axios.get("http://localhost:3001/api/users/")
+    let response = axios.get("/api/users/")
     .catch(err => console.log("error: ", err));
     return response;
   },
   // Gets the user with the given id
   getUser: async function (id) {
-    let response = axios.get("http://localhost:3001/api/users/" + id)
+    let response = axios.get("/api/users/" + id)
     .catch(err => console.log("error: ", err));
     return response;
   },
   // update the user with the given id
   updateUser: async function (id, items) {
-    let response = axios.put("http://localhost:3001/api/users/" + id,{
+    let response = axios.put("/api/users/" + id,{
       projects:items
     })
     .then(x => console.log("the response from updating the user is", x))
@@ -26,14 +26,14 @@ export default {
   },
   // Deletes the user with the given id
   deleteUser: async function (id) {
-    let response = axios.delete("http://localhost:3001/api/users/" + id)
+    let response = axios.delete("/api/users/" + id)
     .then(x => console.log("the response from deleting the user is:", x))
     .catch(err => console.log("error: ", err));
     return response;
   },
   // Saves a user to the database
   saveUser: async function (userData) {
-    let response = axios.post("http://localhost:3001/api/users/", userData)
+    let response = axios.post("/api/users/", userData)
     .then(x => console.log("the response from posting the user is", x))
     .catch(error =>  
       {
