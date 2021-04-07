97f39c715d75c37ae3b72a56f77d696700381f91
DallasWashburn <70284466+DallasWashburn@users.noreply.github.com>
2021-04-07 13:43:09 -0700

    node modules

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,9 +5,9 @@ module.exports = {
   findAllUsers: function(req, res) {
     db.User
     .find(req.query)
-    .sort({ date: -1 })
+    // .sort({ date: -1 })
     .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err + "from controller"));
+    .catch(err => res.status(422).json(err));
   },
   findUserById: function(req, res) {
     db.User
