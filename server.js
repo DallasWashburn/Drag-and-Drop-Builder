17bebc3d336eb70b1f64422d65c60edbbb376e2d
DallasWashburn <70284466+DallasWashburn@users.noreply.github.com>
2021-04-07 16:10:52 -0700

    added headers to api call

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const routes = require("./routes/index");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 const app = express();
 // var multer = require('multer')
 var cors = require('cors');
