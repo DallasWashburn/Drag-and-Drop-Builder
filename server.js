a85bb2e8a1b09d5b37c70c548a9274142e8d93f7
Dallas Washburn <dallas@cybermark.com>
2021-04-07 21:22:49 -0700

    added code for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const routes = require("./routes/index");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 // var multer = require('multer')
 const mongoose = require("mongoose");
